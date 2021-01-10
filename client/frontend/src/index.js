102db0d95d7cf94f75a9f47e110879cf60416f08
Robert Fletcher <robertfletcher@pop-os.localdomain>
2021-01-10 12:31:38 +0000

    Enable PWA support

--- client/frontend/src/index.js
+++ client/frontend/src/index.js
@@ -63,4 +63,4 @@ ReactDOM.render(<Provider store={store()}><App /></Provider>, document.getElemen
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
